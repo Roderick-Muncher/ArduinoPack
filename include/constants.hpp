1fb88669cbd79e35d360a44195a6118f104000e8
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-01-31 14:51:12 -0500

    fix: motor driver code works now

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,8 +2,8 @@
 namespace dt {
 constexpr unsigned int EN_A = 9;
 constexpr unsigned int IN1_A = 2;
-constexpr unsigned int IN1_B = 3;
+constexpr unsigned int IN2_A = 3;
 constexpr unsigned int EN_B = 10;
-constexpr unsigned int IN2_A = 4;
+constexpr unsigned int IN1_B = 4;
 constexpr unsigned int IN2_B = 5;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 L298NX2 drivetrain(dt::EN_A, dt::IN1_A, dt::IN2_A, dt::EN_B, dt::IN1_B, dt::IN2_B);
 
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -13,13 +15,12 @@ void setup() {
     //do nothing
   }
 
-  drivetrain.setSpeedA(120);
-  drivetrain.setSpeedB(80);
+  drivetrain.setSpeedA(255);
+  drivetrain.setSpeedB(255);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  drivetrain.runForA(3000, L298N::FORWARD);
   drivetrain.runForB(3000, L298N::FORWARD);
-
-  Serial.println("Testing (pls work)");
 }
