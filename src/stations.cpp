309e837a3e90c5b89f4dbcfee2db0bdfad4b4685
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-02-01 10:12:19 -0500

    test motor reversing

--- src/stations.cpp
+++ src/stations.cpp
@@ -4,12 +4,12 @@ void runStationA(DDBot& drivetrain) {
   //moveForward(500);
   //Serial.println("Move forward done");
   drivetrain.forward(255);
-  delay(500);
-  drivetrain.right(255);
-  delay(500);
-  drivetrain.left(255);
-  delay(500);
+  delay(1000);
+  drivetrain.clockwise(255);
+  delay(1000);
+  drivetrain.counterClockwise(255);
+  delay(1000);
   drivetrain.backward(255);
-  delay(500);
+  delay(1000);
   drivetrain.stop();
 }
